b761e427cf093990fabd5988452d5d60f931e2d7
Evgenii Khe <kheeugene@gmail.com>
2026-04-16 14:29:13 -0400

    Add a view model for registration

---
+++ resunet/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace resunet.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string? Email { get; set; }
+
+        public string? Password { get; set; } 
+
+        // ViewModel should not know anything about a UserAuth model
+    }
+}

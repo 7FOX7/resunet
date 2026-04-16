c637e19d9916b9cde12cd644f0ed352ee1013362
Evgenii Khe <kheeugene@gmail.com>
2026-04-16 15:34:56 -0400

    Add Register Controller and adjust the code accordingly

---
+++ resunet/Controllers/RegisterController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using resunet.BLL.Auth;
+using resunet.ViewModels;
+using resunet.ViewMappers; 
+
+namespace resunet.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IAuthBLL authBLL; 
+
+        public RegisterController(IAuthBLL authBLL)
+        {
+            this.authBLL = authBLL;
+        }
+
+        [HttpGet]
+        [Route("/register")]
+        public IActionResult Index()
+        {
+            // create an empty view model
+            return View("Index", new RegisterViewModel());    
+        }
+
+        [HttpPost]
+        [Route("/register")]
+        public IActionResult IndexSave(RegisterViewModel registerViewModel)
+        { 
+            if (ModelState.IsValid)
+            {
+                authBLL.CreateUser(AuthMapper.MapRegisterViewModelToUserAuth(registerViewModel)); 
+            }
+
+            return View("Index", registerViewModel); 
+        }
+    }
+}

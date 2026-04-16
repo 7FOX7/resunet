c902be035072ce9edd360d94663af77e961070c8
Evgenii Khe <kheeugene@gmail.com>
2026-04-16 14:29:42 -0400

    Add auth mapper

---
+++ resunet/ViewMappers/AuthMapper.cs
@@ -0,0 +1,20 @@
+﻿using resunet.DAL.Models;
+using resunet.ViewModels;
+
+namespace resunet.ViewMappers
+{
+    public class AuthMapper
+    {
+        public static UserAuth MapRegisterViewModelToUserAuth(RegisterViewModel registerViewModel)
+        {
+            UserAuth userAuth = new UserAuth
+            {
+                // we guarantee that fields are NOT null (validation for nullability will be made in Controller methods)
+                Email = registerViewModel.Email!, 
+                Password = registerViewModel.Password!
+            };
+
+            return userAuth; 
+        }
+    }
+}

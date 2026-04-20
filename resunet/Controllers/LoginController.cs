e96d7dbb5c77d863d3a7a82f89d421391737d896
Evgenii Khe <kheeugene@gmail.com>
2026-04-20 19:57:12 -0400

    Add a login view and controller

---
+++ resunet/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using resunet.BLL.Auth;
+using resunet.DAL.Models;
+using resunet.ViewMappers;
+using resunet.ViewModels;
+
+namespace resunet.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IAuthBLL authBLL;
+
+        // TODO: when there are more services that need to be recalculated on each request, consider creating a separate service registered as scoped / transient. That service will contain all services you want to re-calculate on each request (like httpContextAccessor). You then inject that service here and in other controllers (controller are re-created on each request anyway, so that injected services will be re-created as well)
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public LoginController(IAuthBLL authBLL, IHttpContextAccessor httpContextAccessor)
+        {
+            this.authBLL = authBLL;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet]
+        [Route("/login")]
+        public IActionResult Index()
+        {
+            // create an empty view model
+            return View("Index", new LoginViewModel());
+        }
+
+        [HttpPost]
+        [Route("/login")]
+        public async Task<IActionResult> IndexSave(LoginViewModel loginViewModel)
+        {
+            // check for boolean value (= check if it defaults to 'false' when null)
+            if (ModelState.IsValid)
+            {
+                int id = await authBLL.Authenticate(loginViewModel.Email, loginViewModel.Password, loginViewModel.RememberMe); 
+
+                Login(id);
+                return Redirect("/");
+            }
+
+            return View("Index", loginViewModel);
+        }
+
+        private void Login(int id)
+        {
+            // create a session
+            httpContextAccessor.HttpContext?.Session.SetInt32(AuthConstants.SESSION_USER_ID, id);
+        }
+    }
+}
---
+++ resunet/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace resunet.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty; 
+
+        // bool is non nullable
+        public bool RememberMe { get; set; }
+
+        // ViewModel should not know anything about a UserAuth model
+    }
+}

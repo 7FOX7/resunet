be953019c5ec5ced7c2fb6e48e4833a782016d8c
Evgenii Khe <kheeugene@gmail.com>
2026-04-21 15:20:43 -0400

    Add basic validation

--- resunet/ViewModels/LoginViewModel.cs
+++ resunet/ViewModels/LoginViewModel.cs
@@ -4,10 +4,11 @@ namespace resunet.ViewModels
 {
     public class LoginViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Email is not valid")]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; } = string.Empty; 
 
         // bool is non nullable
--- resunet/ViewModels/RegisterViewModel.cs
+++ resunet/ViewModels/RegisterViewModel.cs
@@ -2,13 +2,21 @@
 
 namespace resunet.ViewModels
 {
-    public class RegisterViewModel
+    public class RegisterViewModel : IValidatableObject
     {
-        [Required]
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Email is not valid")]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
-        public string Password { get; set; } = string.Empty; 
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*-]).{10,}$", ErrorMessage = "Password is too easy to guess")]
+        public string Password { get; set; } = string.Empty;
+
+        // perform additional validation
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            yield break;
+        }
 
         // ViewModel should not know anything about a UserAuth model
     }

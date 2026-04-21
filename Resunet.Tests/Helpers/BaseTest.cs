019e985cd9cad9b3e867b511580b775e4353130d
Evgenii Khe <kheeugene@gmail.com>
2026-04-21 19:13:57 -0400

    Add unit tests

---
+++ Resunet.Tests/Helpers/BaseTest.cs
@@ -0,0 +1,14 @@
+﻿using resunet.BLL.Auth;
+using resunet.DAL;
+
+namespace Resunet.Tests.Helpers
+{
+    public class BaseTest
+    {
+        protected static readonly IAuthDAL authDAL = new AuthDAL();
+        
+        protected static readonly IEncrypt encrypt = new Encrypt();
+
+        protected static readonly IAuthBLL authBLL = new AuthBLL(authDAL, encrypt);
+    }
+}

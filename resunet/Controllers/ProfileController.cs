f708961994d83f4b28eea53a85190eee71679e69
Evgenii Khe <kheeugene@gmail.com>
2026-04-22 22:02:55 -0400

    Add a view for creating profile and adjust the code accordingly

---
+++ resunet/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using resunet.ViewModels;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace resunet.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        [Route("/profile")]
+        public IActionResult Index()
+        {
+            // create an empty view model
+            return View("Index", new ProfileViewModel());
+        }
+    }
+}
---
+++ resunet/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace resunet.ViewModels
+{
+    public class ProfileViewModel
+    {
+        [Required(ErrorMessage = "Profile name is required")]
+        public string ProfileName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; } = string.Empty;
+
+        // ViewModel should not know anything about a UserAuth model
+    }
+}

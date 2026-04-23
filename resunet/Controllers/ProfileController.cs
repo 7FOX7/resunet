e940c306c47681a7560df423256924e68fb472de
Evgenii Khe <kheeugene@gmail.com>
2026-04-22 22:04:57 -0400

    Add an option to upload a profile image

--- resunet/Controllers/ProfileController.cs
+++ resunet/Controllers/ProfileController.cs
@@ -15,5 +15,50 @@ namespace resunet.Controllers
             // create an empty view model
             return View("Index", new ProfileViewModel());
         }
+
+        [HttpPost]
+        [Route("/profile")]
+        public async Task<IActionResult> IndexSave(ProfileViewModel profileViewModel)
+        {
+            // will only be executed if all creds are valid (profile name, first name, last name, etc.)
+            if (ModelState.IsValid && Request.Form.Files.Count > 0)
+            {
+                // get the file 
+                IFormFile file = Request.Form.Files[0];
+
+                // create md5 hash algorithm
+                MD5 md5Hash = MD5.Create();
+                byte[] hashBytes = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(file.FileName));
+
+                // get a hex string
+                string hexString = Convert.ToHexString(hashBytes);
+                // build a dir
+                string dir = new StringBuilder()
+                    .Append("./wwwroot")
+                    .Append("/images/")
+                    .Append($"{hexString.Substring(0, 2)}")          // folder name (2 chars)
+                    .ToString();
+
+                // make sure there's no directory
+                Directory.CreateDirectory(dir); 
+
+                // create a file path which is a combination of wwwroot directory + file name
+                string filePath = new StringBuilder()
+                    .Append(dir)
+                    .Append("/")
+                    .Append(hexString.Substring(3, 4))
+                    .Append("-")
+                    .Append(file.FileName)
+                    .ToString();
+
+                // write to a file
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream); 
+                }
+            }
+
+            return View("Index", profileViewModel);
+        }
     }
 }

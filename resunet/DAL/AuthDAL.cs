c25541942f61c9469c32d2bc6f7b08763d2c3a41
Evgenii Khe <kheeugene@gmail.com>
2026-04-28 15:24:04 -0400

    Refactor DAL methods

--- resunet/DAL/AuthDAL.cs
+++ resunet/DAL/AuthDAL.cs
@@ -1,6 +1,4 @@
 ﻿using resunet.DAL.Models;
-using Dapper;
-using Microsoft.Data.SqlClient;
 
 namespace resunet.DAL
 {
@@ -8,48 +6,35 @@ namespace resunet.DAL
     {
         public async Task<UserAuth> GetUser(string email)
         {
-            using (var connection = new SqlConnection(Environment.GetEnvironmentVariable("CONNECTION_STRING__RESUNET", EnvironmentVariableTarget.User)))
-            {
-                await connection.OpenAsync();
-
-                // return an existing or anonymous user
-                return await connection.QueryFirstOrDefaultAsync<UserAuth>(
-                    @"SELECT UserID, Email, Password, Salt, Status 
+            string sql = @"SELECT UserID, Email, Password, Salt, Status 
                     FROM AppUser 
-                    WHERE Email = @email", new { email = email }
-                ) ?? new UserAuth(); 
-            }
+                    WHERE Email = @email
+                ";
+
+            // return an existing or anonymous user
+            return (await DbHelper.ExecuteQueryAsync<UserAuth>(sql, new { email = email })).FirstOrDefault() ?? new UserAuth();
         }
 
         public async Task<UserAuth> GetUser(int id)
         {
-            using (var connection = new SqlConnection(Environment.GetEnvironmentVariable("CONNECTION_STRING__RESUNET", EnvironmentVariableTarget.User)))
-            {
-                await connection.OpenAsync();
-
-                // return an existing or anonymous user
-                return await connection.QueryFirstOrDefaultAsync<UserAuth>(
-                    @"SELECT UserID, Email, Password, Salt, Status 
+            string sql = @"SELECT UserID, Email, Password, Salt, Status 
                     FROM AppUser 
-                    WHERE UserID = @id", new { id = id }
-                ) ?? new UserAuth();
-            }
+                    WHERE UserID = @id
+                ";
+
+            // return an existing or anonymous user
+            return (await DbHelper.ExecuteQueryAsync<UserAuth>(sql, new { id = id })).FirstOrDefault() ?? new UserAuth();
         }
 
         // create a user based on the model and return their id
         public async Task<int> CreateUser(UserAuth userAuth)
         {
-            using (var connection = new SqlConnection(Environment.GetEnvironmentVariable("CONNECTION_STRING__RESUNET", EnvironmentVariableTarget.User)))
-            {
-                await connection.OpenAsync();
-
-                string sql = @"INSERT INTO AppUser(Email, Password, Salt, Status) 
+            string sql = @"INSERT INTO AppUser(Email, Password, Salt, Status) 
                     OUTPUT inserted.UserID
                     VALUES(@Email, @Password, @Salt, @Status)
                 ";
 
-                return await connection.QueryFirstOrDefaultAsync<int>(sql, userAuth); 
-            }
+            return await DbHelper.ExecuteScalarAsync(sql, userAuth); 
         }
     }
 }
---
+++ resunet/DAL/DbHelper.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace resunet.DAL
+{
+    public class DbHelper
+    {
+        // scalar accepts one or more params and returns exactly one value
+        public static async Task<int> ExecuteScalarAsync(string sql, object model)
+        {
+            using (var connection = new SqlConnection(Environment.GetEnvironmentVariable("CONNECTION_STRING__RESUNET", EnvironmentVariableTarget.User)))
+            {
+                await connection.OpenAsync();
+
+                return await connection.QueryFirstOrDefaultAsync<int>(sql, model);
+            }
+        }
+
+        // return MULTIPLE results of type T (you can then return a single or the entire collection to the client in DAL methods)
+        public static async Task<IEnumerable<T>> ExecuteQueryAsync<T>(string sql, object model)  
+        {
+            using (var connection = new SqlConnection(Environment.GetEnvironmentVariable("CONNECTION_STRING__RESUNET", EnvironmentVariableTarget.User)))
+            {
+                await connection.OpenAsync();
+
+                return await connection.QueryAsync<T>(sql, model);
+            }
+        }
+    }
+}
